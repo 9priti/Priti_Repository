e26f5ae236b34c13758d1464135758f0282cbcb6
pprasad3 <pprasad3@CSCINDAE752510.asiapac.globalcsc.net>
2015-10-08 11:48:50 +0530

    Added couple of classes

---
+++ src/com/org/priti/test/AnagramCheck.java
@@ -0,0 +1,25 @@
+package com.org.priti.test;
+import java.util.Arrays;
+
+public class AnagramCheck {
+	
+	public static void main (String args [])
+	{
+		String originalString = "test";
+		String testString = "rriti";
+		
+		checkAnagram (originalString, testString);
+		System.out.println(checkAnagram (originalString, testString));
+	}
+
+	public static boolean checkAnagram (String original , String test)
+	{
+		char [] originalArray = original.toCharArray();
+		char [] testArray = test.toCharArray();
+		Arrays.sort(originalArray);
+		Arrays.sort(testArray);
+		
+		return Arrays.equals(originalArray, testArray);
+	}
+	
+}
