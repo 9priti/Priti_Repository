bbda3aec048cf75bb6602bdadf51f6551d316f1c
pprasad3 <pprasad3@CSCINDAE752510.asiapac.globalcsc.net>
2015-11-22 13:43:39 +0530

    Factorail with recursion program

---
+++ src/com/org/priti/test/FactorialWithRecursion.java
@@ -0,0 +1,22 @@
+package com.org.priti.test;
+import java.util.Scanner;
+
+public class FactorialWithRecursion {
+
+	public static void main(String[] args) {
+		System.out.println ("Enter the number whose factorial is to be calculated :");
+		Scanner sc = new Scanner (System.in);
+		int count = sc.nextInt();
+		
+		System.out.println("Factorial of "+ count +" is "+ factorial (count));
+	}
+
+	private static int factorial(int count) {
+		
+		if (count == 0 || count == 1)
+		{
+			return 1;
+		}
+			return count*factorial(count-1) ;
+	}
+}
