8069685afc50cbd50928fa98d661c2312e89a046
Priti Sinha <9priti0901@gmail.com>
2015-12-14 19:35:50 +0530

    Factory Pattern Demo

---
+++ PracticeWorkspace1/src/AbstractTest.java
@@ -0,0 +1,9 @@
+
+public abstract class AbstractTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!");
+
+	}
+
+}
---
+++ PracticeWorkspace1/src/subclass.java
@@ -0,0 +1,20 @@
+public class subclass extends superclass {
+ 
+superclass superClass = new superclass ();
+public void displayResult()
+{
+	
+}
+ 
+
+
+ 
+public static void main(String args[]) {
+ 
+subclass obj=new subclass();
+ 
+obj.displayResult();
+ 
+}
+ 
+}
